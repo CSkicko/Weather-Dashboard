d338f0d5d6240a4d889debaeae711b2a8a837cae
Craig <craig.skicko@gmail.com>
2022-06-29 09:16:46 +1000

    Added checks for incorrect entries and issues in fetch response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,38 @@
 var searchBtn = document.getElementById("search-button");
 var searchInput = document.getElementById("searched-city");
 
-var createGeocode = function(event){
+var getWeatherData = function(event){
     event.preventDefault();
     var searchItem = searchInput.value;
     var apiUrl = "http://api.positionstack.com/v1/forward?access_key=55515964d41906e81966df595b39b4f2&query=" + searchItem;
-    fetch(apiUrl)
+    if (searchItem){
+        fetch(apiUrl)
         .then(function (response) {
-            return response.json();
+            if (response.status === 200){
+                return response.json();
+            }
+            else {
+                alert("Cannot find resource, please try again.");
+            }
         })
         .then(function (data) {
             // console.log(data.data)
-            var latitude = data.data[0].latitude;
-            var longitude = data.data[0].longitude;
-            console.log("Latitude: " + latitude);
-            console.log("Longitude: " + longitude);
+            if (data.data.length){
+                var latitude = data.data[0].latitude;
+                var longitude = data.data[0].longitude;
+                var coordinates = [latitude, longitude];
+                retrieveForecast(coordinates);
+            } else {
+                alert("City not found, please enter a valid city");
+            }
+            
         })
+    }
 }
 
-searchBtn.addEventListener("click",createGeocode);
+var retrieveForecast = function(coords){
+    console.log(coords);
+
+}
+
+searchBtn.addEventListener("click", getWeatherData);
