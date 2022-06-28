b5a42b434e6c1025adf1dc09a29cb1618b574aca
Craig <craig.skicko@gmail.com>
2022-06-29 09:32:26 +1000

    Added functionality to pass the city name to the retrieve forecast function and get and display the data to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,33 @@ var getWeatherData = function(event){
     var apiUrl = "http://api.positionstack.com/v1/forward?access_key=55515964d41906e81966df595b39b4f2&query=" + searchItem;
     if (searchItem){
         fetch(apiUrl)
+            .then(function (response) {
+                if (response.status === 200){
+                    return response.json();
+                }
+                else {
+                    alert("Cannot find resource, please try again.");
+                }
+            })
+            .then(function (data) {
+                // console.log(data.data)
+                if (data.data.length){
+                    var latitude = data.data[0].latitude;
+                    var longitude = data.data[0].longitude;
+                    var coordinates = [latitude, longitude];
+                    retrieveForecast(coordinates, searchItem);
+                } else {
+                    alert("City not found, please enter a valid city");
+                }
+            })
+    }
+}
+
+var retrieveForecast = function(coords, city){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords[0] + "&lon=" + coords[1] + "&exclude=minutely,hourly,alerts&appid=f660a3811e9a5d90a12e993e669272c0"
+    console.log(apiUrl);
+    console.log(city);
+    fetch(apiUrl)
         .then(function (response) {
             if (response.status === 200){
                 return response.json();
@@ -14,26 +41,10 @@ var getWeatherData = function(event){
             else {
                 alert("Cannot find resource, please try again.");
             }
+        }) 
+        .then(function(data) {
+            console.log(data);
         })
-        .then(function (data) {
-            // console.log(data.data)
-            if (data.data.length){
-                var latitude = data.data[0].latitude;
-                var longitude = data.data[0].longitude;
-                var coordinates = [latitude, longitude];
-                retrieveForecast(coordinates);
-            } else {
-                alert("City not found, please enter a valid city");
-            }
-            
-        })
-    }
-}
-
-var retrieveForecast = function(coords){
-    console.log(coords);
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords[0] + "&lon=" + coords[1] + "&exclude=minutely,hourly,alerts&appid=f660a3811e9a5d90a12e993e669272c0"
-    console.log(apiUrl);
 }
 
 searchBtn.addEventListener("click", getWeatherData);
